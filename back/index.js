c1174d06e871b356a905d00de6d5d71c50b5c164
deborah-powers <deborah.powers89@gmail.com>
2017-12-19 15:59:47 +0100

    mes modifications

--- back/index.js
+++ back/index.js
@@ -11,114 +11,138 @@ var username= "";
 var password= "";
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+	extended: false
 }));
 
 app.use(function(req, res, next) {
-  res.header(`Access-Control-Allow-Origin`, `*`);
-  res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
-  res.header(`Access-Control-Allow-Headers`, `Content-Type`);
-  next();
+	res.header(`Access-Control-Allow-Origin`, `*`);
+	res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+	res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+	next();
 });
 
 function randomToken() {
-  var string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var result = '';
-  for (var i = 0; i < 32; i++) {
-    result += string[Math.floor(Math.random() * (string.length - 1))];
-  }
-  return result;
+	var string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	var result = '';
+	for (var i = 0; i < 32; i++) {
+		result += string[Math.floor(Math.random() * (string.length - 1))];
+	}
+	return result;
 }
 
 function tokenCheck(token) {
-  var result = tokenList.find(function (element) {
-    return element == token;
-  });
-  return result;
-}
-
-function findUser(name) {
-  var db = _client.db(dbName);
-    db.collection('UlanBator').find({name}).toArray(function(err, docs){
-    return docs.name == username;
-  });
+	var result = tokenList.find(function (element) {
+		return element == token;
+	});
+	return result;
 }
 
 app.post('/login', function (req, res) {
-  var body = req.body;
+	var body = req.body;
 
-  if (body.username && body.password) {
-    var db = _client.db(dbName);
-    db.collection('UlanBator').find({username : body.username}).toArray(function(err, docs) {
-      if (docs) {
-        doc = docs[0];
+	if (body.username && body.password) {
+		var db = _client.db(dbName);
+		db.collection('UlanBator').find({username : body.username}).toArray(function(err, docs) {
+			if (docs) {
+				doc = docs[0];
 
-        if(doc.password == body.password) {
-          tokenList.push(randomToken());
+				if(doc.password == body.password) {
+					tokenList.push(randomToken());
 
-          res.status(200).send({message: 'Login good', token: tokenList});
-          console.log('ok')
-        } else {
-          res.status(412).send('Wrong password');
-          }
-        } 
-      else {
-          res.status(404).send('This username does not exist'); 
-        }
-    });
-  } else {
-    res.status(412).send('Wrong username');
-  }
+					res.status(200).send({message: 'Login good', token: tokenList});
+					console.log('ok')
+				} else {
+					res.status(412).send('Wrong password');
+					}
+				} 
+			else {
+					res.status(404).send('This username does not exist'); 
+				}
+		});
+	} else {
+		res.status(412).send('Wrong username');
+	}
 });
 
+
 app.post ('/create-account', function (req, res){
-//create-account sert à intégrer de nouveaux users
-    var Busername = req.body.username;
-    var Bpassword = req.body.password;
-  // si les parametres ont ete donne
-  if (Busername && Bpassword){
-    console.log(findUser(Busername))
-    if (findUser(Busername)){
-    // Si le profil existe déjà
-      res.status(409).send ('This profile already exist !');
-    }
-    else {
-    // Si le profil n'existe pas
-      var newUser = {
-        //On créer un objet qui contient les paramètres
-        username: Busername,
-        password: Bpassword,
-      };
-          // mettre le profil sur la bdd
-      function profilInBdd (bdd){
-        var collection = bdd.collection ('UlanBator');
-        collection.insertOne (newUser, function(error, result){
-          if (error) console.log ("This profile cannot be stored in the database");
-          else console.log ("Profile stored in database")
-            res.status(200).send ('Profile created');
-        });
-      }
-      //On appelle la fonction
-      var myDb = _client.db(dbName);
-      profilInBdd (myDb);
-          }
-  }
-  else {
-  // S'il manque une des deux infos requises
-    res.status(404).send ("You should enter a password and an username");
-  }
+	//create-account sert à intégrer de nouveaux users
+	var Busername = req.body.username;
+	var Bpassword = req.body.password;
+	function findUser(){
+		var db = _client.db (dbName);
+		var myUser =0;
+		var listUsers =[];
+		var nb= false;
+		/*
+		cursorUsers est un cursor
+		https://docs.mongodb.com/manual/tutorial/iterate-a-cursor/#read-operations-cursors
+		forEach de mongo pour lire les cursors
+		j'utilise find apres avoir echoue a utiliser findOne
+
+		console.log (listUsers.length);
+		if (listUsers) myUser = listUsers[0];
+		*/
+
+		var cursorUsers = db.collection('UlanBator').find ({ username: Busername, password: Bpassword });
+		// tranformer le cursor en liste
+		cursorUsers.forEach (function (myDoc){
+			listUsers.push (myDoc);
+			nb= true;
+			console.log (nb);
+		});
+		console.log (nb);
+		/*
+		var nb = db.collection('UlanBator').count ({ username: Busername, password: Bpassword });
+		var cursorUsers = db.collection('UlanBator').find ({ username: Busername, password: Bpassword });
+		console.log ('nb '+ myUser);
+		*/
+		// recuperer le premier element
+		return listUsers;
+	}
+	// si les parametres ont ete donne
+	if (Busername && Bpassword){
+		// Si le profil existe déjà
+		if (findUser()) res.status(409).send ('This profile already exist !');
+		else {
+		// Si le profil n'existe pas
+			//On créer un objet qui contient les paramètres
+			var newUser = {
+				username: Busername,
+				password: Bpassword,
+			};
+					// mettre le profil sur la bdd
+			function profilInBdd (bdd){
+				var collection = bdd.collection ('UlanBator');
+				collection.insertOne (newUser, function(error, result){
+					if (error) console.log ("This profile cannot be stored in the database");
+					else{
+						console.log ("Profile stored in database");
+						res.status(200).send ('Profile created');
+					}
+				});
+			}
+			//On appelle la fonction
+			var myDb = _client.db(dbName);
+			profilInBdd (myDb);
+		}
+	}
+	else {
+	// S'il manque une des deux infos requises
+		res.status(404).send ("You should enter a password and an username");
+	}
 });
 
 
 
 MongoClient.connect(url, function (err, client) {
 
-  if (err) console.log('Erro! ', err);
-  else {
-    console.log("Connected successfully to server");
-    app.listen(3000, function () {
-      console.log('Listening on port 3000');
-      });
-    _client = client;
-     }
+	if (err) console.log('Erro! ', err);
+	else {
+		console.log("Connected successfully to server");
+		app.listen(3000, function () {
+			console.log('Listening on port 3000');
+			});
+		_client = client;
+		 }
 });
--- front/create-account/create-controller.js
+++ front/create-account/create-controller.js
@@ -1,10 +1,8 @@
 angular.module ('notepad').controller ('createController', function ($scope, $state, $http){
 	var loginUrl = 'http://localhost:3000/create-account';
 	$scope.sendData = function(){
-		console.log($scope.username);
-		console.log($scope.password);
-		$http.post (loginUrl, {username: $scope.username , password: $scope.password}).then (function (response){
-			console.log('good');
+		$http.post (loginUrl, {username: $scope.username , password: $scope.password})
+		.then (function (response){
 			$state.go('login');
 		});
 	}
